88ac097b3271acbed993629755cf819c8f102fd5
Chantal Keller <ckeller@lmf.cnrs.fr>
2024-05-30 20:16:57 +0200

    Try to compile C callbacks

---
+++ src/extraction/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <caml/callback.h>
+
+/* TODO: try to remove [extern] */
+extern char* dummy_checker();
+
+int main(int argc, char ** argv)
+{
+  char* result;
+
+  /* Initialize OCaml code */
+  caml_startup(argv);
+  /* Do some computation */
+  result = dummy_checker();
+  printf("%s\n", result);
+  return 0;
+}
---
+++ src/extraction/test-callbacks/mod.ml
@@ -0,0 +1,13 @@
+(* Testing callbacks from C to OCaml
+   see https://ocaml.org/manual/5.2/intfc.html#s:c-advexample
+ *)
+
+
+let rec fib n = if n < 2 then 1 else Add.add (fib(n-1)) (fib(n-2))
+
+let format_result n = Printf.sprintf "Result is: %d\n" n
+
+(* Export those two functions to C *)
+
+let _ = Callback.register "fib" fib
+let _ = Callback.register "format_result" format_result
